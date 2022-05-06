0fe27f437cb9bef0e3fa850dfcef9355089d8bba
Harsh Rathod <68862779+harshrathod5341@users.noreply.github.com>
2022-05-06 03:34:01 -0400

    Add files via upload

---
+++ Mad_libs.py
@@ -0,0 +1,10 @@
+print("----------- THE MAD LIBS -----------")
+
+# Take input from users
+noun = input("Enter the noun: ")
+plural_noun = input("Enter the plural noun: ")
+verb = input("Enter the verb: ")
+
+print("Today, every student has a computer small enough to fit into his " + noun + ".")
+print("He can solve any math problem by simply pushing the computers little " + plural_noun + ".")
+print("They can also " + verb + " better than human.")
